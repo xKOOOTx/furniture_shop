d232f99c206fc6e514a75aed693abb8a2518f3fe
KOOOT <kooot123@gmail.com>
2021-08-18 03:57:39 +0300

    переделываю шапку, остановился на кнопке бургер, откуда там этот паддинг снизу?

--- burgerScript.js
+++ burgerScript.js
@@ -1,1 +1,8 @@
 const burgerClose = document.getElementById('burgerClose');
+
+burgerClose.addEventListener('click', toggleBurger())
+
+function toggleBurger() {
+    const mobileMenu = document.querySelector('.header__mobileNav');
+    mobileMenu.classList.toggle('.active')
+}
